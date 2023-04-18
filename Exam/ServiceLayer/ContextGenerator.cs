ad7ea75c8790f09530bdb9e069336eca3b3fbc7a
StefanMavrov <stefanrmavrov@gmail.com>
2023-04-18 21:51:43 +0300

    Podobrenie

---
+++ Exam/ServiceLayer/ContextGenerator.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer;
+
+namespace ServiceLayer
+{
+    public class ContextGenerator
+    {
+        private static ExamDbContext examcontext;
+        private static DocumentContext documentcontext;
+
+        public static ExamDbContext CreateDbContext()
+        {
+            examcontext = new ExamDbContext();
+            return examcontext;
+        }
+
+        public static ExamDbContext GetDbContext()
+        {
+            return examcontext;
+        }
+        public static DocumentContext CreateDocumentContext(ExamDbContext dbContext)
+        {
+            documentcontext = new DocumentContext(dbContext);
+            return documentcontext;
+        }
+        public static DocumentContext GetDocumentContext()
+        {
+            return documentcontext;
+        }
+    }
+}
---
+++ Exam/ServiceLayer/DocumentManager.cs
@@ -0,0 +1,85 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer
+{
+    public class DocumentManager
+    {
+        private readonly DocumentContext documentContext;
+
+        public DocumentManager(DocumentContext documentContext)
+        {
+            this.documentContext = documentContext;
+        }
+
+        public async Task CreateAsync(Document document)
+        {
+            try
+            {
+                await documentContext.CreateAsync(document);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<Document> ReadAsync(string entrynumber, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                return await documentContext.ReadAsync(entrynumber, useNavigationalProperties);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<Document>> ReadAllAsync(bool useNavigationalProperties = false)
+        {
+            try
+            {
+                return await documentContext.ReadAllAsync(useNavigationalProperties);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task UpdateAsync(Document document, bool useNavigationalProperties = false)
+        {
+            try
+            {
+                await documentContext.UpdateAsync(document, useNavigationalProperties);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task DeleteAsync(string entrynumber)
+        {
+            try
+            {
+                await documentContext.DeleteAsync(entrynumber);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
